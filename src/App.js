35d422268a3d4ec2633be203c1424a243ae6b05b
Will1184 <brandonwilliammg@gmail.com>
2023-06-21 08:30:08 -0600

    seventh-commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{Fragment }  from 'react';
 import'./Styles/App.css';
-import { BrowserRouter as Router ,Route,Routes} from 'react-router-dom';
+import { BrowserRouter as Router ,Route,Routes, HashRouter} from 'react-router-dom';
 import Futbolista from './component/Futbolista';
 import Login from './pages/Login';
 import ProtectedRoutes from './component/ProtectedRoutes';
@@ -12,8 +12,9 @@ function App() {
   return (
     <Fragment>
       <Router>       
-       <Routes>
-       <Route path='/' element={<Login/>}/>                    
+        <HashRouter basename='/'>
+        <Routes>
+       <Route exact path='/' element={<Login/>}/>                    
        <Route element={<ProtectedRoutes/>}>
        <Route path='/options' element={<Options/>}/>
        <Route path='/home' element={<Home/>}/>   
@@ -21,7 +22,8 @@ function App() {
        <Route path='/newfutbolista' element={<Futbolista/>}/>
        <Route path='/futbolista/:id' element={<Futbolista/>}/>        
       </Route>
-       </Routes>             
+       </Routes>
+        </HashRouter>             
       </Router>             
     </Fragment>    
   );
